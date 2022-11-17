e81ae004ac201bc553ac6510b9a9ff462850a714
unknown <eric-1209@t-online.de>
2022-11-17 17:51:52 +0100

    lehreinheit 2

---
+++ src/components/knopf/Knopf.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./knopf.css"
+
+export default function Knopf() {
+  return (
+    <>
+        <button className='button red-button'>rot</button>
+        <button className='button blue-button'>blau</button>
+    </>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Knopf from '../../components/knopf/Knopf'
 import Zähler from '../../components/zähler/Zähler'
 import "./home.css"
 
 export default function Home() {
-  
+
+    const [color,setColor] = useState(null)//usestate ändern => react lädt das element neu
+    // = setzen
+    // == ????
+    // === vergleichen
+
+
+    // frage ? tue wenn wahr : tue wenn nicht wahr
+
     return (
     <main className='home'>
         <section>
             <div className="container">
-                <h1 className='headline'>Willkommen zu meiner ersten Website!</h1>
+                <h1 className={color === "blue" ? 'headline blue' : "headline red"}>Willkommen zu meiner ersten Website!</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt delectus illum ea qui animi earum corrupti? Cumque at officiis laborum!</p>
             </div>        
         </section>
         <section className='container'>
-            <Zähler/>
+            {/* <Zähler/> */}
+            {/* <Knopf/> */}
+            <button className='button' onClick={() => {setColor(current => current === "blue" ? "red" : "blue")}}>Farbwechsel</button>
         </section>
         <section>
             
